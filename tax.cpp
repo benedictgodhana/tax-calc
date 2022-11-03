ec4f8ed2efcdadd7f753c36ad2137bd2bf3a9239
benedictgodhana <113383009+benedictgodhana@users.noreply.github.com>
2022-11-03 19:54:10 +0300

    Add files via upload

---
+++ tax.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  //declare variables
+  int Gross salary, Net salary, Tax payable, Tax rate
+    //input varriables 
+    Gross salary = 10, 000;
+  Tax rate = (30 % -25 %);
+  Tax payable = (Tax rate * Gross rate);
+  Net salary = Gross rate - Tax payable;
+
+  //calculations
+
+  Tax payable = (30 % -25 %) * Gross salary;
+  Net salary = Gross salary - Tax Payable;
+
+  //output
+cout << Tax payable >> endl;
+cout << Net salary >> endl;
+}
+
+return 0;
+}
+
+
